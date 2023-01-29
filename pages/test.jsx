ad72846321971dede8b96c4b48dd9386cc61d579
fbn776@gmail.com <fbn776@gmail.com>
2023-01-30 00:51:02 +0530

    Test

---
+++ pages/test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import bg from '../components/Home/Modules/Background.jsx'
+import '../styles/globals.css'
+
+ReactDOM.render(
+	bg(),
+	document.getElementById('react-app')
+);
