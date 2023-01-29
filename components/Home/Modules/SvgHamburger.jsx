362994c6d3bf520ae9be949796bfe9d654700af0
fbn776@gmail.com <fbn776@gmail.com>
2023-01-29 17:36:35 +0530

    Tweaked the hamburger animations

--- components/Home/Modules/SvgHamburger.jsx
+++ components/Home/Modules/SvgHamburger.jsx
@@ -27,9 +27,9 @@ function SvgHamburger({action}){
 	}} style={{
 		padding: '10px'
 	}}><svg style={{overflow: 'visible'}} className={burger_class} width={25} height={20} viewBox="0 0 25 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-	  <path d="M0 3C0 1.34315 1.34315 0 3 0H25V1C25 2.65685 23.6569 4 22 4H0V3Z" fill="white" />
-		<path d="M0 11C0 9.34315 1.34315 8 3 8H25V9C25 10.6569 23.6569 12 22 12H0V11Z" fill="white" />
-  	<path d="M0 19C0 17.3431 1.34315 16 3 16H25V17C25 18.6569 23.6569 20 22 20H0V19Z" fill="white" />
+	  <path className="bar-1" d="M0 3C0 1.34315 1.34315 0 3 0H25V1C25 2.65685 23.6569 4 22 4H0V3Z" fill="white" />
+		<path className="bar-2" d="M0 11C0 9.34315 1.34315 8 3 8H25V9C25 10.6569 23.6569 12 22 12H0V11Z" fill="white" />
+  	<path className="bar-3" d="M0 19C0 17.3431 1.34315 16 3 16H25V17C25 18.6569 23.6569 20 22 20H0V19Z" fill="white" />
 	</svg></div>);
 }
 
