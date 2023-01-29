cae4809eae05a51b7e53a17b862ffe8aa19d10ae
fbn776@gmail.com <fbn776@gmail.com>
2023-01-29 18:18:35 +0530

    Restructured, Tweaked

--- pages/comingsoon.jsx
+++ pages/comingsoon.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import React from 'react';
+import comingSoonBG from '../public/coming_soon_bg.png'
+import rituLogo from '../public/logo_white_sym.png';
+import '../styles/coming-soon.css'
+import '../components/Utils/SvgHamburger.css'
+import SvgHamburger from '../components/Utils/SvgHamburger.jsx'
 
-const comingsoon = () => {
+
+function Background() {
 	return (
-		<>
-  	  <div>comingsoon</div>
-  	  <h1>Test 001</h1>
-    </>
+		<div className="bg">
+  		<img src={comingSoonBG}/>
+	  </div>);
+}
+
+function Header() {
+	let [burgerState, setBugerState] = React.useState(true);
+	return (<div className="header">
+    <div className="logo">
+      <img src={rituLogo} />
+      RITU
+    </div>
+    <div className="nav">
+    	<SvgHamburger action={setBugerState}/>
+      <div className="nav-items">
+        <label>Home</label>
+        <label>Events</label>
+        <label>Workshops</label>
+        <label>Contact</label>
+      </div>
+      <SideMenu state={burgerState}/>
+    </div>
+  </div>);
+}
+
+function SideMenu({ state }) {
+	let cls = state ? "side-nav closed" : "side-nav opened";
+	return (
+		<div className={cls}>
+      <label>Home</label>
+    	<label>Events</label>
+      <label>Workshops</label>
+      <label>Contact</label>
+		</div>
 	)
 }
 
-export default comingsoon
+const comingsoon = () => {
+	return (
+		<div>
+			<Background/>
+			<Header/>
+			<div className="body">
+			   <div>COMING SOON</div>
+			</div>
+    </div>
+	)
+}
+
+export default comingsoon;
